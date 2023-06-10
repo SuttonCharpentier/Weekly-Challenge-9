43a238d94d7caa9b97a10f781d9010c1b8bda9a2
unknown <suttoncharpentier@gmail.com>
2023-06-10 12:03:39 -0700

    fixed issues with generated readme

---
+++ README.md
@@ -0,0 +1,60 @@
+# readme generator
+  <br>    
+
+## License
+![License](https://img.shields.io/badge/License-MIT-green.svg)
+
+Follow link for more informations  https://opensource.org/licenses/MIT
+
+ This project is licensed under the MIT license.
+Licensed under the MIT License.
+you may not use this file except in compliance with the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+## Description
+
+generates a readme
+
+## Table of Contents
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  
+  * [Contributing](#contributing)
+  
+  * [Test](#test)
+  
+  * [Questions](#questions)
+     
+## Installation
+  To install necessary dependencies, run the following command:
+
+     npm i
+
+## Usage
+
+
+
+
+## Contributing
+
+
+
+## Test
+
+To run the test, run the following command:
+
+    npm test
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me direcly at s@s.com.
+You can find more of my work at https://github.com/s.  
+
+©2023 S. Confidential and Proprietary. All Rights Reserved.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,82 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Variables
+const dateOb = new Date();
+const year = dateOb.getFullYear();
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Returns a license badge based on which license is passed in if there is no license, return an empty string
+function renderLicenseBadge(license) {
+ return license === 'None' ? ' ' : `![License](https://img.shields.io/badge/License-${license}-green.svg)\n`
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Returns the license link  if there is no license, return an empty string
+function renderLicenseLink(license) {
+  return license === 'None' ? ' ' : `${'Follow link for more informations'}  https://opensource.org/licenses/${license.replace('_', '-')}\n`
+}
 
-// TODO: Create a function to generate markdown for README
+// Returns the license section of README if there is no license, return an empty string
+function renderLicenseSection(license) {
+  return license === 'None' ? ' ' : `
+## License
+${renderLicenseBadge(license)}
+${renderLicenseLink(license)}
+ This project is licensed under the ${license.replace('_', ' ')} license.
+Licensed under the ${license.replace('_', ' ')} License.
+you may not use this file except in compliance with the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+`
+}
+
+// Generates markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  <br>    
+${renderLicenseSection(data.license)}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  
+  * [Contributing](#contributing)
+  
+  * [Test](#test)
+  
+  * [Questions](#questions)
+     
+## Installation
+  To install necessary dependencies, run the following command:
+
+     ${data.command}
+
+## Usage
+
+${data.usage}
+
+
+## Contributing
+
+${data.contributing}
+
+## Test
+
+To run the test, run the following command:
+
+    ${data.test}
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me direcly at ${data.email}.
+You can find more of my work at https://github.com/${data.username}.  
+
+©${year} ${data.username.toUpperCase()}. Confidential and Proprietary. All Rights Reserved.
 `;
 }
 
